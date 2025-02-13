67f89767627958d00611f017e621e242526302a6
Matthew Cloutier <mrcloutier624@gmail.com>
2025-02-13 17:50:54 -0500

    changed color, added image (don't forget to cite)

--- IQP_Tester/Main.Designer.cs
+++ IQP_Tester/Main.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.tabMainControl = new System.Windows.Forms.TabControl();
             this.tabHistory = new System.Windows.Forms.TabPage();
             this.HistoryCeasecu = new System.Windows.Forms.Label();
@@ -35,28 +36,33 @@
             this.KidsLifeFood = new System.Windows.Forms.Label();
             this.KidsLifeToys = new System.Windows.Forms.Label();
             this.lblUptime = new System.Windows.Forms.Label();
+            this.pictureBoxRomaniaFlag = new System.Windows.Forms.PictureBox();
             this.tabMainControl.SuspendLayout();
             this.tabHistory.SuspendLayout();
             this.tabKidsLife.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRomaniaFlag)).BeginInit();
             this.SuspendLayout();
             // 
             // tabMainControl
             // 
             this.tabMainControl.Controls.Add(this.tabHistory);
             this.tabMainControl.Controls.Add(this.tabKidsLife);
-            this.tabMainControl.Location = new System.Drawing.Point(12, 12);
+            this.tabMainControl.Location = new System.Drawing.Point(32, 29);
+            this.tabMainControl.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.tabMainControl.Name = "tabMainControl";
             this.tabMainControl.SelectedIndex = 0;
-            this.tabMainControl.Size = new System.Drawing.Size(756, 473);
+            this.tabMainControl.Size = new System.Drawing.Size(2016, 1128);
             this.tabMainControl.TabIndex = 0;
             // 
             // tabHistory
             // 
+            this.tabHistory.Controls.Add(this.pictureBoxRomaniaFlag);
             this.tabHistory.Controls.Add(this.HistoryCeasecu);
-            this.tabHistory.Location = new System.Drawing.Point(4, 22);
+            this.tabHistory.Location = new System.Drawing.Point(10, 48);
+            this.tabHistory.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.tabHistory.Name = "tabHistory";
-            this.tabHistory.Padding = new System.Windows.Forms.Padding(3);
-            this.tabHistory.Size = new System.Drawing.Size(748, 447);
+            this.tabHistory.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.tabHistory.Size = new System.Drawing.Size(1996, 1070);
             this.tabHistory.TabIndex = 0;
             this.tabHistory.Text = "History";
             this.tabHistory.UseVisualStyleBackColor = true;
@@ -65,9 +71,10 @@
             // 
             this.HistoryCeasecu.AutoSize = true;
             this.HistoryCeasecu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HistoryCeasecu.Location = new System.Drawing.Point(106, 165);
+            this.HistoryCeasecu.Location = new System.Drawing.Point(125, 372);
+            this.HistoryCeasecu.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.HistoryCeasecu.Name = "HistoryCeasecu";
-            this.HistoryCeasecu.Size = new System.Drawing.Size(214, 25);
+            this.HistoryCeasecu.Size = new System.Drawing.Size(521, 61);
             this.HistoryCeasecu.TabIndex = 0;
             this.HistoryCeasecu.Text = "Who was Ceasescu?";
             this.HistoryCeasecu.Click += new System.EventHandler(this.HistoryCeasecu_Click);
@@ -76,10 +83,11 @@
             // 
             this.tabKidsLife.Controls.Add(this.KidsLifeFood);
             this.tabKidsLife.Controls.Add(this.KidsLifeToys);
-            this.tabKidsLife.Location = new System.Drawing.Point(4, 22);
+            this.tabKidsLife.Location = new System.Drawing.Point(10, 48);
+            this.tabKidsLife.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
             this.tabKidsLife.Name = "tabKidsLife";
-            this.tabKidsLife.Padding = new System.Windows.Forms.Padding(3);
-            this.tabKidsLife.Size = new System.Drawing.Size(748, 447);
+            this.tabKidsLife.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.tabKidsLife.Size = new System.Drawing.Size(1996, 1070);
             this.tabKidsLife.TabIndex = 1;
             this.tabKidsLife.Text = "Kid\'s Life";
             this.tabKidsLife.UseVisualStyleBackColor = true;
@@ -88,9 +96,10 @@
             // 
             this.KidsLifeFood.AutoSize = true;
             this.KidsLifeFood.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.KidsLifeFood.Location = new System.Drawing.Point(480, 137);
+            this.KidsLifeFood.Location = new System.Drawing.Point(1280, 327);
+            this.KidsLifeFood.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.KidsLifeFood.Name = "KidsLifeFood";
-            this.KidsLifeFood.Size = new System.Drawing.Size(192, 25);
+            this.KidsLifeFood.Size = new System.Drawing.Size(465, 61);
             this.KidsLifeFood.TabIndex = 1;
             this.KidsLifeFood.Text = "What did they eat?";
             this.KidsLifeFood.Click += new System.EventHandler(this.KidsLifeFood_Click);
@@ -99,9 +108,10 @@
             // 
             this.KidsLifeToys.AutoSize = true;
             this.KidsLifeToys.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.KidsLifeToys.Location = new System.Drawing.Point(24, 137);
+            this.KidsLifeToys.Location = new System.Drawing.Point(64, 327);
+            this.KidsLifeToys.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.KidsLifeToys.Name = "KidsLifeToys";
-            this.KidsLifeToys.Size = new System.Drawing.Size(269, 25);
+            this.KidsLifeToys.Size = new System.Drawing.Size(657, 61);
             this.KidsLifeToys.TabIndex = 0;
             this.KidsLifeToys.Text = "What Toys Did Kids Have?";
             this.KidsLifeToys.Click += new System.EventHandler(this.KidsLifeToys_Click);
@@ -109,22 +119,33 @@
             // lblUptime
             // 
             this.lblUptime.AutoSize = true;
-            this.lblUptime.Location = new System.Drawing.Point(539, 9);
+            this.lblUptime.Location = new System.Drawing.Point(1437, 21);
+            this.lblUptime.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
             this.lblUptime.Name = "lblUptime";
-            this.lblUptime.Size = new System.Drawing.Size(80, 13);
+            this.lblUptime.Size = new System.Drawing.Size(207, 32);
             this.lblUptime.TabIndex = 1;
             this.lblUptime.Text = "UNAVAILABLE";
             // 
+            // pictureBoxRomaniaFlag
+            // 
+            this.pictureBoxRomaniaFlag.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRomaniaFlag.Image")));
+            this.pictureBoxRomaniaFlag.Location = new System.Drawing.Point(639, 174);
+            this.pictureBoxRomaniaFlag.Name = "pictureBoxRomaniaFlag";
+            this.pictureBoxRomaniaFlag.Size = new System.Drawing.Size(895, 578);
+            this.pictureBoxRomaniaFlag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxRomaniaFlag.TabIndex = 1;
+            this.pictureBoxRomaniaFlag.TabStop = false;
+            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(783, 511);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(2088, 1219);
             this.ControlBox = false;
             this.Controls.Add(this.lblUptime);
             this.Controls.Add(this.tabMainControl);
-            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Main";
             this.ShowIcon = false;
             this.Text = "Educating Romanian Children";
@@ -133,6 +154,7 @@
             this.tabHistory.PerformLayout();
             this.tabKidsLife.ResumeLayout(false);
             this.tabKidsLife.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRomaniaFlag)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,6 +169,7 @@
         private System.Windows.Forms.Label KidsLifeFood;
         private System.Windows.Forms.Label HistoryCeasecu;
         private System.Windows.Forms.Label lblUptime;
+        private System.Windows.Forms.PictureBox pictureBoxRomaniaFlag;
     }
 }
 
