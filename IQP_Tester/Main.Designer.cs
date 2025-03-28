6e6bd3e378b9277ab97b5b27101b2b73fdacae75
Matthew Cloutier <mrcloutier624@gmail.com>
2025-03-28 16:29:20 +0200

    adding some logos

--- IQP_Tester/Main.Designer.cs
+++ IQP_Tester/Main.Designer.cs
@@ -28,8 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.btnLanguage = new System.Windows.Forms.Button();
             this.panelRegimeFall = new System.Windows.Forms.Panel();
+            this.pbRevolution = new System.Windows.Forms.PictureBox();
             this.lblHowDidTheRegimeFall = new System.Windows.Forms.Label();
             this.btnCredits = new System.Windows.Forms.Button();
             this.panelOppression = new System.Windows.Forms.Panel();
@@ -42,14 +44,13 @@
             this.lblStories = new System.Windows.Forms.Label();
             this.panelThenAndNow = new System.Windows.Forms.Panel();
             this.lblThenAndNow = new System.Windows.Forms.Label();
-            this.pbRevolution = new System.Windows.Forms.PictureBox();
             this.panelRegimeFall.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRevolution)).BeginInit();
             this.panelOppression.SuspendLayout();
             this.panelLifeUnder.SuspendLayout();
             this.panelTimeline.SuspendLayout();
             this.panelStories.SuspendLayout();
             this.panelThenAndNow.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbRevolution)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLanguage
@@ -75,6 +76,18 @@
             this.panelRegimeFall.Size = new System.Drawing.Size(192, 84);
             this.panelRegimeFall.TabIndex = 5;
             this.panelRegimeFall.Click += new System.EventHandler(this.panelRegimeFall_Click);
+            // 
+            // pbRevolution
+            // 
+            this.pbRevolution.Image = global::IQP_Tester.Properties.Resources.Romanian_Revolution;
+            this.pbRevolution.InitialImage = global::IQP_Tester.Properties.Resources.Romanian_Revolution;
+            this.pbRevolution.Location = new System.Drawing.Point(6, 34);
+            this.pbRevolution.Margin = new System.Windows.Forms.Padding(1);
+            this.pbRevolution.Name = "pbRevolution";
+            this.pbRevolution.Size = new System.Drawing.Size(337, 261);
+            this.pbRevolution.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbRevolution.TabIndex = 2;
+            this.pbRevolution.TabStop = false;
             // 
             // lblHowDidTheRegimeFall
             // 
@@ -219,18 +232,6 @@
             this.lblThenAndNow.TabIndex = 0;
             this.lblThenAndNow.Text = "Then and Now";
             // 
-            // pbRevolution
-            // 
-            this.pbRevolution.Image = global::IQP_Tester.Properties.Resources.Romanian_Revolution;
-            this.pbRevolution.InitialImage = global::IQP_Tester.Properties.Resources.Romanian_Revolution;
-            this.pbRevolution.Location = new System.Drawing.Point(6, 34);
-            this.pbRevolution.Margin = new System.Windows.Forms.Padding(1);
-            this.pbRevolution.Name = "pbRevolution";
-            this.pbRevolution.Size = new System.Drawing.Size(337, 261);
-            this.pbRevolution.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbRevolution.TabIndex = 2;
-            this.pbRevolution.TabStop = false;
-            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -245,12 +246,14 @@
             this.Controls.Add(this.panelThenAndNow);
             this.Controls.Add(this.panelRegimeFall);
             this.Controls.Add(this.btnLanguage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(1);
             this.Name = "Main";
             this.Text = "Educating Romanian Children";
             this.Resize += new System.EventHandler(this.Main_Resize);
             this.panelRegimeFall.ResumeLayout(false);
             this.panelRegimeFall.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRevolution)).EndInit();
             this.panelOppression.ResumeLayout(false);
             this.panelOppression.PerformLayout();
             this.panelLifeUnder.ResumeLayout(false);
@@ -261,7 +264,6 @@
             this.panelStories.PerformLayout();
             this.panelThenAndNow.ResumeLayout(false);
             this.panelThenAndNow.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pbRevolution)).EndInit();
             this.ResumeLayout(false);
 
         }
