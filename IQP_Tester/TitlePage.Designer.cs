c5a92cb694f1a345694ff5d6ad7623a99a1d541c
Matthew Cloutier <mrcloutier624@gmail.com>
2025-04-14 15:13:06 +0300

    titlepage background fix

--- IQP_Tester/TitlePage.Designer.cs
+++ IQP_Tester/TitlePage.Designer.cs
@@ -32,17 +32,17 @@
             this.lblTitle = new System.Windows.Forms.Label();
             this.lblSubtitle = new System.Windows.Forms.Label();
             this.tableLayoutTitleMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutLanguageTitlePageBtnAlign = new System.Windows.Forms.TableLayoutPanel();
+            this.btnLanguage = new System.Windows.Forms.Button();
             this.pbBackground3 = new System.Windows.Forms.PictureBox();
             this.pbBackground2 = new System.Windows.Forms.PictureBox();
             this.pbBackground1 = new System.Windows.Forms.PictureBox();
-            this.tableLayoutLanguageTitlePageBtnAlign = new System.Windows.Forms.TableLayoutPanel();
-            this.btnLanguage = new System.Windows.Forms.Button();
             this.tableLayoutTitle.SuspendLayout();
             this.tableLayoutTitleMain.SuspendLayout();
+            this.tableLayoutLanguageTitlePageBtnAlign.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbBackground3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbBackground2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbBackground1)).BeginInit();
-            this.tableLayoutLanguageTitlePageBtnAlign.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutTitle
@@ -110,6 +110,36 @@
             this.tableLayoutTitleMain.Size = new System.Drawing.Size(1904, 1041);
             this.tableLayoutTitleMain.TabIndex = 12;
             // 
+            // tableLayoutLanguageTitlePageBtnAlign
+            // 
+            this.tableLayoutLanguageTitlePageBtnAlign.ColumnCount = 3;
+            this.tableLayoutLanguageTitlePageBtnAlign.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 94F));
+            this.tableLayoutLanguageTitlePageBtnAlign.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.25F));
+            this.tableLayoutLanguageTitlePageBtnAlign.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0.75F));
+            this.tableLayoutLanguageTitlePageBtnAlign.Controls.Add(this.btnLanguage, 1, 1);
+            this.tableLayoutLanguageTitlePageBtnAlign.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutLanguageTitlePageBtnAlign.Location = new System.Drawing.Point(3, 939);
+            this.tableLayoutLanguageTitlePageBtnAlign.Name = "tableLayoutLanguageTitlePageBtnAlign";
+            this.tableLayoutLanguageTitlePageBtnAlign.RowCount = 3;
+            this.tableLayoutLanguageTitlePageBtnAlign.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57F));
+            this.tableLayoutLanguageTitlePageBtnAlign.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.25F));
+            this.tableLayoutLanguageTitlePageBtnAlign.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 0.75F));
+            this.tableLayoutLanguageTitlePageBtnAlign.Size = new System.Drawing.Size(1898, 99);
+            this.tableLayoutLanguageTitlePageBtnAlign.TabIndex = 33;
+            // 
+            // btnLanguage
+            // 
+            this.btnLanguage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnLanguage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnLanguage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLanguage.Location = new System.Drawing.Point(1787, 59);
+            this.btnLanguage.Name = "btnLanguage";
+            this.btnLanguage.Size = new System.Drawing.Size(93, 35);
+            this.btnLanguage.TabIndex = 8;
+            this.btnLanguage.Text = "English";
+            this.btnLanguage.UseVisualStyleBackColor = false;
+            this.btnLanguage.Click += new System.EventHandler(this.btnLanguage_Click);
+            // 
             // pbBackground3
             // 
             this.pbBackground3.BackColor = System.Drawing.Color.Transparent;
@@ -146,41 +176,11 @@
             this.pbBackground1.TabStop = false;
             this.pbBackground1.Visible = false;
             // 
-            // tableLayoutLanguageTitlePageBtnAlign
-            // 
-            this.tableLayoutLanguageTitlePageBtnAlign.ColumnCount = 3;
-            this.tableLayoutLanguageTitlePageBtnAlign.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 94F));
-            this.tableLayoutLanguageTitlePageBtnAlign.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.25F));
-            this.tableLayoutLanguageTitlePageBtnAlign.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0.75F));
-            this.tableLayoutLanguageTitlePageBtnAlign.Controls.Add(this.btnLanguage, 1, 1);
-            this.tableLayoutLanguageTitlePageBtnAlign.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutLanguageTitlePageBtnAlign.Location = new System.Drawing.Point(3, 939);
-            this.tableLayoutLanguageTitlePageBtnAlign.Name = "tableLayoutLanguageTitlePageBtnAlign";
-            this.tableLayoutLanguageTitlePageBtnAlign.RowCount = 3;
-            this.tableLayoutLanguageTitlePageBtnAlign.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57F));
-            this.tableLayoutLanguageTitlePageBtnAlign.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.25F));
-            this.tableLayoutLanguageTitlePageBtnAlign.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 0.75F));
-            this.tableLayoutLanguageTitlePageBtnAlign.Size = new System.Drawing.Size(1898, 99);
-            this.tableLayoutLanguageTitlePageBtnAlign.TabIndex = 33;
-            // 
-            // btnLanguage
-            // 
-            this.btnLanguage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.btnLanguage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnLanguage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLanguage.Location = new System.Drawing.Point(1787, 59);
-            this.btnLanguage.Name = "btnLanguage";
-            this.btnLanguage.Size = new System.Drawing.Size(93, 35);
-            this.btnLanguage.TabIndex = 8;
-            this.btnLanguage.Text = "English";
-            this.btnLanguage.UseVisualStyleBackColor = false;
-            this.btnLanguage.Click += new System.EventHandler(this.btnLanguage_Click);
-            // 
             // TitlePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(1904, 1041);
             this.Controls.Add(this.pbBackground3);
@@ -196,10 +196,10 @@
             this.tableLayoutTitle.ResumeLayout(false);
             this.tableLayoutTitle.PerformLayout();
             this.tableLayoutTitleMain.ResumeLayout(false);
+            this.tableLayoutLanguageTitlePageBtnAlign.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbBackground3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbBackground2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbBackground1)).EndInit();
-            this.tableLayoutLanguageTitlePageBtnAlign.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
